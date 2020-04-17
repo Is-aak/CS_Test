eb2e0d77f0f7f94f2ea1229b5a883d456c3a55c6
Is-aak <62933096+Is-aak@users.noreply.github.com>
2020-04-17 09:07:25 +0200

    Titel zentriert

--- Zahlensysteme/Zahlensysteme/Zahlensysteme/Program.cs
+++ Zahlensysteme/Zahlensysteme/Zahlensysteme/Program.cs
@@ -2,14 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+//Programm zum Umwandeln von verschiedenen Zahlensysteme
 namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Titel("Dies ist ein Titel");
+
             Console.ReadKey();
+        }
+
+
+        static void Titel(string text)
+        {
+            int TxtLen = text.Length;
+            Console.Clear();
+            for (int i = 0; i < Console.WindowWidth; i++ )
+            {
+                Console.Write("-");
+            }
+            for (int i = 0; i < ( Console.WindowWidth - TxtLen) / 2; i++) 
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine(text);
+
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("-");
+            }
         }
     }
 }
